130eebf1797339abdfe428bab73d67a2cd124f3f
om.p.shahane@gmail.com <om.p.shahane@gmail.com>
2023-02-07 09:34:37 +0530

    subtotal

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,13 +1,9 @@
 import React from "react";
 import "./Subtotal.css";
-import CurrencyFormat from 'react-currency-format'
 function Subtotal({ value }) {
   return (
     <div className="subtotal">
-      <CurrencyFormat
-        renderText={(value) => (
-          <>
-            <p>
+     <p>
               Subtotal 0 items:
               <br />
               <strong>{value}</strong>
@@ -17,14 +13,6 @@ function Subtotal({ value }) {
               This order contains a gift
             </small>
             <button className="checkout__button">Checkout</button>
-          </>
-        )}
-        decimalScale={2}
-        value={value}
-        displayType={"text"}
-        thousandSeperator={true}
-        prefix={"$"}
-      />
     </div>
   );
 }
