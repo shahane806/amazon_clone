c37ed71a9f800387f3dcae7ff7a1d2d4ff4c2290
om.p.shahane@gmail.com <om.p.shahane@gmail.com>
2023-02-07 09:42:03 +0530

    s

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./Subtotal.css";
-function Subtotal({ value }) {
+
+function Subtotal({value}) {
   return (
     <div className="subtotal">
-     <p>
-              Subtotal 0 items:
-              <br />
+    
+            <p>
+              Subtotal 0 items:<br/>
               <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" />
               This order contains a gift
             </small>
-            <button className="checkout__button">Checkout</button>
+            <button className="checkout__button">
+                Checkout
+            </button>
+          
     </div>
   );
 }
